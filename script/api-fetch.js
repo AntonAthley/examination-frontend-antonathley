40156bf099fe435fbb4d71e5d05eadfd4161b2fd
AntonAthley <antonathley@gmail.com>
2025-01-05 14:15:37 +0100

    init fetch api

---
+++ script/api-fetch.js
@@ -0,0 +1,41 @@
+const API_KEY = "43ac502565e2855ce307337744abfc71";
+const BASE_URL = "https://api.themoviedb.org/3/movie";
+
+async function fetchFromEndpoint(endpoint) {
+  try {
+    const response = await fetch(`${BASE_URL}/${endpoint}?api_key=${API_KEY}`);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error(`Error fetching ${endpoint}:`, error);
+    return null;
+  }
+}
+
+async function fetchMovieLists() {
+  try {
+    const endpoints = ["popular", "top_rated"];
+
+    const results = await Promise.all(
+      endpoints.map((endpoint) => fetchFromEndpoint(endpoint))
+    );
+
+    const [popular, topRated] = results;
+
+    // console.log({
+    //   popular,
+    //   topRated,
+    // });
+
+    return {
+      popular,
+      topRated,
+    };
+  } catch (error) {
+    console.error("Error in fetchMovieLists:", error);
+    return null;
+  }
+}
+
+// fetchMovieLists();
